e59adfb484d15d5d3fb050e5ef9030b154afe3c1
Benhanan14406 <a.b.abinaya.fuun@gmail.com>
2025-10-08 14:36:54 +0700

    Temporary Cabang Olahraga and Following models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class CabangOlahraga(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=255, default="", editable=False)
+
+class Following(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    priority = models.IntegerField(editable=True)
