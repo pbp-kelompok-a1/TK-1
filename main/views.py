df3860b6ab4ff819b40c29f2c524cf03987576c1
ilh4msh <ilhamputrahasim@gmail.com>
2025-10-22 23:09:39 +0700

    feature:register, login, logout

--- main/views.py
+++ main/views.py
@@ -2,53 +2,52 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from django.contrib import messages
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from main.models import Event, EventType 
-from main.forms import EventForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+import datetime
 
-# Create your views here.
-def event_list(request):
 
-    upcoming_events = Event.objects.filter(
-        start_time__gte=timezone.now()
-    ).select_related('creator') 
-    
+def register(request):
+    form = UserCreationForm()
 
-    user_is_logged_in = request.user.is_authenticated
-    is_admin = request.user.is_staff
-    
-    context = {
-        'events': upcoming_events,
-        'user_is_logged_in': user_is_logged_in,
-        'is_admin': is_admin,
-        
-        'EVENT_TYPE_GLOBAL': EventType.GLOBAL,
-        'EVENT_TYPE_COMMUNITY': EventType.COMMUNITY,
-    }
-    
-    return render(request, 'events_list.html', context)
-
-@login_required
-def event_create(request):
-    if not request.user.is_authenticated:
-        messages.error(request, "You must be logged in to create a Community Event.")
-        return redirect('event_list')s
-
-    if request.method == 'POST':
-        form = EventForm(request.POST)
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            event = form.save(commit=False)
-            event.creator = request.user
-            event.event_type = EventType.COMMUNITY 
-            event.save()
-            
-            messages.success(request, f"Tournament '{event.title}' created successfully!")
-            return redirect('event_list')
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.error(request, 'Invalid username or password. Please try again.')
     else:
-        form = EventForm()
-    
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+def show_main(request):
     context = {
-        'form': form,
-        'page_title': "Create Community Tournament"
+        'last_login': request.COOKIES.get('last_login', 'Never')
     }
-    return render(request, 'event_create_form.html', context)
+    return render(request, "main.html", context)
+
