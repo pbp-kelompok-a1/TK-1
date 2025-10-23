b9045a0be83af0e0d087ecf3fbbcfbb76b3cf380
nichovesakha81 <studynicho@gmail.com>
2025-10-23 22:20:04 +0700

    simpan kerjaan CRUD AJAX, implement js JSON, modal, toast pada modul profil atlet

--- profil_atlet/admin.py
+++ profil_atlet/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Atlet, Medali
 
 # Register your models here.
+admin.site.register(Atlet)
+admin.site.register(Medali)
--- profil_atlet/views.py
+++ profil_atlet/views.py
@@ -4,6 +4,10 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import Atlet
 from .forms import AtletForm # import form 
 from django.db.models import Count, Q
+from django.http import JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
 
 # fungsi cek Admin 
 # utk jadi Admin harus dipastikan punya status 'is_superuser' 
@@ -14,7 +18,7 @@ def is_admin(user):
 
 # R (Read) utk List Atlet
 def list_atlet(request):
-    # buat field baru 'medal_count'
+    # buat field 'medal_count'
     base_query = Atlet.objects.annotate(
         # count medali emas
         gold_count=Count('medali', filter=Q(medali__medal_type='Gold Medal')),
@@ -40,7 +44,7 @@ def detail_atlet(request, pk):
     atlet = get_object_or_404(Atlet, pk=pk)
     
     # JIKA user BUKAN admin DAN atletnya diset 'is_visible=False'
-    # penjaga agar hanya Admin yang bisa melihat profil atlet yang sedang disembunyikan. 
+    # ini sbg penjaga agar hanya Admin yang bisa melihat profil atlet yang sedang disembunyikan. 
     # jika seorang Member (bukan admin) mencoba mengakses URL detail atlet yang disembunyikan, 
     # mereka akan ditolak dan dikembalikan ke halaman daftar.
     if not request.user.is_superuser and not atlet.is_visible:
@@ -60,15 +64,14 @@ def detail_atlet(request, pk):
 # C (Create) only Admin
 @user_passes_test(is_admin) # decorator yang memanggil fungsi is_admin
 def create_atlet(request):
-    if request.method == 'POST':
-        form = AtletForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('profil_atlet:list_atlet')
-    else:
-        form = AtletForm()
-        
-    context = {'form': form, 'page_title': 'Tambah Atlet Baru'}
+    if not request.user.is_superuser:
+        return redirect('profil_atlet:list_atlet')
+    
+    form = AtletForm()
+    context = {
+        'form': form,
+        'page_title': 'Tambah Atlet'
+    }
     return render(request, 'profil_atlet/form_atlet.html', context)
 
 # U (Update) only Admin
@@ -95,3 +98,65 @@ def delete_atlet(request, pk):
         return redirect('profil_atlet:list_atlet') 
         
     return render(request, 'profil_atlet/confirm_delete.html', {'atlet': atlet})
+
+# function view utk  data JSON
+def show_json_atlet(request):
+    # kita ambil query yg sama persis dgnview list_atlet
+    base_query = Atlet.objects.annotate(
+        gold_count=Count('medali', filter=Q(medali__medal_type='Gold Medal')),
+        silver_count=Count('medali', filter=Q(medali__medal_type='Silver Medal')),
+        bronze_count=Count('medali', filter=Q(medali__medal_type='Bronze Medal')),
+        total_medals=Count('medali')
+    )
+
+    if request.user.is_authenticated and request.user.is_superuser:
+        atlet_list = base_query.order_by('name')
+    else:
+        atlet_list = base_query.filter(is_visible=True).order_by('name')
+        
+    data = []
+    for atlet in atlet_list:
+        data.append({
+            'pk': atlet.pk,
+            'name': atlet.name,
+            'short_name': atlet.short_name,
+            'discipline': atlet.discipline,
+            'country': atlet.country,
+            'is_visible': atlet.is_visible,
+            'gold_count': atlet.gold_count,
+            'silver_count': atlet.silver_count,
+            'bronze_count': atlet.bronze_count,
+            'total_medals': atlet.total_medals,
+        })
+
+    return JsonResponse(data, safe=False)
+
+@csrf_exempt
+def create_atlet_ajax(request):
+    if request.method == "POST":
+        # tdk bisa pake request.POST biasa karena data dikirim sebagai JSON/FormData
+        
+        form = AtletForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            # sinyal sukses
+            return JsonResponse({"status": "success", "message": "Athlete added successfully!"}, status=201)
+        else:
+            # sinyal error beserta daftar error-nya
+            return JsonResponse({"status": "error", "message": "Invalid form", "errors": form.errors}, status=400)
+
+    # jika ada yg akses via GET, kirim error
+    return JsonResponse({"status": "error", "message": "GET method is not allowed"}, status=405)
+
+@csrf_exempt 
+def delete_atlet_ajax(request, pk):
+    if request.method == "DELETE":
+        try:
+            atlet = get_object_or_404(Atlet, pk=pk)
+            atlet.delete()
+            return JsonResponse({"status": "success", "message": "Athlete deleted successfully"}, status=200)
+        except Exception as e:
+            return JsonResponse({"status": "error", "message": str(e)}, status=500)
+
+    return JsonResponse({"status": "error", "message": "Method is not allowed"}, status=405)
