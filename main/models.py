4f56dff82a174ba51ff4559276ae9f780ef39f4a
Benhanan14406 <a.b.abinaya.fuun@gmail.com>
2025-10-23 18:25:31 +0700

    Created a custom User model, using ImageField for the profile picture.;

--- main/models.py
+++ main/models.py
@@ -5,3 +5,12 @@ import uuid
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class User(models.Model):
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE, editable=False)
+    username = models.CharField(max_length=100, editable=False)
+    name = models.TextField(editable=True)
+    picture = models.ImageField(null=True, editable=True)
+
+    def __str__(self):
+        return self.username
