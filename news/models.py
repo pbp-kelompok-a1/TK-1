d00cc9b9935eb04e030e5aef3b1a8294076a6dae
Benhanan14406 <a.b.abinaya.fuun@gmail.com>
2025-10-23 21:20:09 +0700

    Following sort fix

--- news/models.py
+++ news/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from following.models import CabangOlahraga
+
 class Berita(models.Model):
 
     CATEGORY_CHOICES = [
@@ -15,7 +17,7 @@ class Berita(models.Model):
     content = models.TextField()
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='other')
     thumbnail = models.URLField(blank=True, null=True)
-
+    cabangOlahraga = models.ForeignKey(CabangOlahraga, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
 
