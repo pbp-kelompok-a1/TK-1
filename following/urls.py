407d9cb26e8f667657081d90bb9e1e6b7d078dab
Benhanan14406 <a.b.abinaya.fuun@gmail.com>
2025-12-01 14:20:57 +0700

    Forgot the URLs

--- following/urls.py
+++ following/urls.py
@@ -5,5 +5,8 @@ from .views import *
 urlpatterns = [
     path('profile/<int:userId>', profilePage, name='profile'),
     path('unfollow/<uuid:follow_id>/', unfollow, name='unfollow'),
-    path('createSport/', createCabangOlahraga, name='createSport')
+    path('createSport/', createCabangOlahraga, name='createSport'),
+    path('showJSONFollowing/', getJSONFollowing, name='showJSONFollowing'),
+    path('showJSONCabangOlahraga/', getJSONCabangOlahraga, name='showJSONCabangOlahraga'),
+    path('showJSONCustomUser/', getJSONCustomUser, name='showJSONCustomUser'),
 ]
