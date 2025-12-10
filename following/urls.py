5a456da2252b5fff91bab13997f624ffc5aa1631
Benhanan14406 <a.b.abinaya.fuun@gmail.com>
2025-12-10 19:39:47 +0700

    Tweak in profile page

--- following/urls.py
+++ following/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import *
 
 urlpatterns = [
-    path('profile/<int:userId>', profilePage, name='profile'),
+    path('profile/', profilePage, name='profile'),
     path('unfollow/<uuid:follow_id>/', unfollow, name='unfollow'),
     path('createSport/', createCabangOlahraga, name='createSport'),
     path('showJSONFollowing/', getJSONFollowing, name='showJSONFollowing'),
